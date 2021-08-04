af24d925b09814a8b010252b42410e850a67e855
Antonio Mesa Casares <amesa.casares@gmail.com>
2021-08-04 21:18:49 +0200

    Agregados todos los archivos del proyecto inicial

---
+++ archive.php
@@ -0,0 +1,24 @@
+<?php get_header();?>
+
+    <div class="container my-4">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1><?php the_archive_title();?></h1>
+            </div>
+            <?php if (have_posts()) {
+                while(have_posts()){
+                    the_post();?>
+                    <div class="col-4 text-center-single-archive">
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail( 'large');?>
+                            <h4><?php the_title(); ?></h4>
+                        </a>
+                    </div>
+                    <?php
+                }
+            }?>
+
+        </div>
+    </div>
+
+<?php get_footer();?>
