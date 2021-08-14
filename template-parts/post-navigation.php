2a4d7512060ce5b4dc2ca186b8587886e3f6462d
cybermito <cybermito@gmail.com>
2021-08-14 07:52:37 +0200

    Confirmar el commit de la carpeta template-parts

---
+++ template-parts/post-navigation.php
@@ -0,0 +1,9 @@
+<div class="row my-5">
+    <div class="col-6">
+        <?php previous_post_link(); ?>
+    </div>
+    <div class="col-6">
+        <?php next_post_link(); ?>
+    </div>
+
+</div>
