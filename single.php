5f0d327418258e076151d2eb6535e580a6f1a6a1
cybermito <cybermito@gmail.com>
2021-08-17 07:03:21 +0200

    Clase 7 y 8 modificando el UI del Post Type personalizado (producto)

--- single.php
+++ single.php
@@ -1,16 +1,17 @@
 <?php get_header(); ?>
 
 <main class='container my-3'>
+    <h1> <?php the_title() ?> </h1>
     <?php if(have_posts()){
             while(have_posts()){
                 the_post();
             ?>
-                <h1 class='my-5'><?php the_title() ?></h1>
-                <div class="row">
-                    <div class="col-4">
+                
+                <div class="row my-5">
+                    <div class="col-md-6 col-12">
                         <?php the_post_thumbnail('large'); ?>
                     </div>
-                    <div class="col-8">
+                    <div class="col-md-6 col-12">
                         <?php the_content(); ?>
                     </div> 
                 </div>
