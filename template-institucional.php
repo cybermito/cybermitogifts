bb7c8453794a5f9ed3a7209d1e382e0191bef064
cybermito <cybermito@gmail.com>
2021-08-21 08:05:01 +0200

    Clase 11 ¿Que es un page template y como se utiliza?

---
+++ template-institucional.php
@@ -0,0 +1,20 @@
+<?php 
+//Template Name: Página Institucional
+get_header(); 
+$fields = get_fields(); //Obtenemos en un array todos los campos personalizados de la plantilla. 
+?>
+
+<main class='container my-5'>
+    <h1 class='my-3'><?php echo $fields['titulo']; ?></h1> <!-- Vamos llamando a cada campo personalizado y poniéndolo donde nos interese --> 
+    <?php if(have_posts()){
+            while(have_posts()){
+                the_post();?>
+                <img src="<?php echo $fields['imagen']; ?>" alt="Logo representativo sobre nosotros"> 
+                <hr>        
+                <?php the_content(); ?>
+
+         <?php }
+    }?>
+</main>
+
+<?php get_footer(); ?>
